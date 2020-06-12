5376d10aa23b5f6ddc5dc6f722ff395ed9cdc135
Zainab Azzam <zainab_azzam@yahoo.com>
2020-06-12 10:56:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,25 @@
  const reduceToTotal = function (sourceArray, startingPoint){
    let result = sourceArray.reduce()
  }
+ function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){  
+        if(accumulator == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return sourceArray.reduce(reducer, false)
+} 
