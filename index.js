4cbce56272223301306e78dc50e6724564d6b8c0
Zainab Azzam <zainab_azzam@yahoo.com>
2020-06-12 10:55:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+ const mapToNegativize = function (sourceArray){
+   let result = sourceArray.map(x => x* -1)
+   return result
+ }
+ const mapToNoChange = function (sourceArray){
+  let result = sourceArray.map(x => x)
+   return result
+   
+ }
+  const mapToDouble = function (sourceArray){
+  let result = sourceArray.map(x => x*2)
+   return result
+   
+ }
+  const mapToSquare = function (sourceArray){
+  let result = sourceArray.map(x => Math.pow(x, 2))
+   return result
+   
+ }
+ 
+ const reduceToTotal = function (sourceArray, startingPoint){
+   let result = sourceArray.reduce()
+ }
